d7e290ace08a7ecb9be59f1c6be3670fc10df9f9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-15 23:49:21 +0000

    Add R/InstallPackages.R

---
+++ R/InstallPackages.R
@@ -0,0 +1,29 @@
+# Title     : Install Packages
+# Objective : Install Packages
+# Created by: Think
+# Created on: 15/11/2021
+
+InstallPackages <- function (){
+
+  packages <- c('tidyverse', 'data.table', 'ggplot2', 'rmarkdown', 'rstatix', 'latex2exp')
+
+  # Install
+  .install <- function(x){
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      if (x == 'rmarkdown') {tinytex::install_tinytex()}
+    }
+  }
+  lapply(packages, .install)
+
+  # Activate
+  .activate <- function (x){
+    library(x, character.only = TRUE)
+    if (x == 'rmarkdown') {library(tinytex)}
+  }
+  lapply(packages[!(packages %in% c('tidyverse', 'healthcareai', 'equatiomatic'))], .activate)
+
+  # Active libraries
+  sessionInfo()
+
+}
