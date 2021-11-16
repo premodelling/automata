e8f4d7d8c6abe0d5c44b707f3c55ed186aac5458
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-16 16:25:38 +0000

    Update R/functions/AutomataMatrix.R

--- R/functions/AutomataMatrix.R
+++ R/functions/AutomataMatrix.R
@@ -3,7 +3,18 @@
 # Created by: Think
 # Created on: 15/11/2021
 
-#' Automata Matrix Function
+#' Automata Matrix
+#'
+#' @description Creates a cellular automata matrix w.r.t. a automata rule function, a
+#'              starting array, and an iterations value that dictates the number of
+#'              automata arrays.
+#'
+#' @param iterations: the number of cellular automata arrays to be created
+#' @param tensor: the starting array
+#' @param FUN: the update rule function
+#'
+#' @return a cellular automata matrix/pattern
+#'
 AutomataMatrix <- function (iterations, tensor, FUN) {
 
   T <- tensor
