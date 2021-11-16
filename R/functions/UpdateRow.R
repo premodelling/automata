44b8918b6c644bb8e61ed029cb9ceceb91b4b6ae
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-16 01:20:03 +0000

    Update R/functions/UpdateRow.R

--- R/functions/UpdateRow.R
+++ R/functions/UpdateRow.R
@@ -3,23 +3,33 @@
 # Created by: Think
 # Created on: 15/11/2021
 
+
 #' @param tensor: the row vector that will update the next row
 #' @param FUN: the function of an automata rule type
 UpdateRow <- function (tensor, FUN) {
 
-  # The length of the tensor
+
+  # The length of the tensor (N) and a
+  # cyclically shifted vector of indices (R) for ...
   N <- length(tensor)
+  R <- c(N, seq(from = 1, to = N - 1))
 
-  # A cyclically shifted vector of indices for ...
-  reference <- c(N, seq(from = 1, to = N - 1))
 
-  # A function that evaluates the value of the cell parallel to the cell in focus
-  ParallelCell <- function(index) {
-    coordinates <- tensor[c(reference[index], index, (index %% N) + 1)]
+  #' A private function that evaluates the state of a cell parallel
+  #' to the one in focus
+  #'
+  #' @param index: cell index
+  #'
+  #' @return state of parallel cell
+  #'
+  .UpdateCell <- function(index) {
+    coordinates <- tensor[c(R[index], index, (index %% N) + 1)]
     state <- FUN(i = coordinates[1], j = coordinates[2], k = coordinates[3])
+
     return(state)
   }
 
-  return( mapply(ParallelCell, index = seq(from = 1, to = N)) )
+
+  return( mapply(.UpdateCell, index = seq(from = 1, to = N)) )
 
 }
