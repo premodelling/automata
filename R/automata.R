6662a42930db849a148e6b3465bad3cc466527c7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 15:33:35 +0000

    Update R/automata.R

--- R/automata.R
+++ R/automata.R
@@ -6,14 +6,15 @@
 source(file = 'R/functions/AutomataRule.R')
 source(file = 'R/functions/UpdateCell.R')
 
-source(file = 'functions/UpdateRowIndependent.R')
-source(file = 'functions/UpdateRowDependent.R')
-source(file = 'functions/UpdateTensor.R')
+source(file = 'R/functions/UpdateRowIndependent.R')
+source(file = 'R/functions/UpdateRowDependent.R')
+source(file = 'R/functions/UpdateTensor.R')
 
 source(file = 'R/functions/AutomataMatrix.R')
 source(file = 'R/functions/AutomataModuloRule.R')
 source(file = 'R/functions/AutomataRandomRule.R')
 source(file = 'R/functions/AutomataSummationRule.R')
+source(file = 'R/functions/AutomataExperiment.R')
 
 
 # Testing AutomataRule
@@ -53,60 +54,36 @@ image(pattern, col = c('white', 'black'), axes = FALSE)
 
 
 
-# Testing Random Starting Tensors
+# Random Starting Tensors
 N <- 50
 tensor <- sample(x = 0:1, size = N, replace = TRUE, prob = c('0' = 0.5, '1' = 0.5))
-pattern <- AutomataMatrix(iterations = 25, tensor = tensor, FUN = AutomataRule)
+randomstart <- AutomataMatrix(iterations = 25, tensor = tensor, FUN = AutomataRule)
 
-print(pattern)
-image(pattern, col = c('white', 'black'), axes = FALSE)
+print(randomstart)
+image(randomstart, col = c('white', 'black'), axes = FALSE)
 
 
 
-# Testing AutomataModuloRule
+# New Rules
+
+# AutomataModuloRule
 mapply(AutomataModuloRule,
        i = c(0, 0, 0, 0, 1, 1, 1, 1),
        j = c(0, 0, 1, 1, 0, 0, 1, 1),
        k = c(0, 1, 0, 1, 0, 1, 0, 1))
 
-N <- 50
-tensor <- sample(x = 0:1, size = N, replace = TRUE, prob = c('0' = 0.6, '1' = 0.4))
-pattern <- AutomataMatrix(iterations = 25, tensor = tensor, FUN = AutomataModuloRule)
-
-print(pattern)
-image(pattern, col = c('white', 'black'), axes = FALSE)
-
-
-# Testing AutomataRandomRule
+# AutomataRandomRule
 mapply(AutomataRandomRule,
        i = c(0, 0, 0, 0, 1, 1, 1, 1),
        j = c(0, 0, 1, 1, 0, 0, 1, 1),
        k = c(0, 1, 0, 1, 0, 1, 0, 1))
 
-N <- 50
-tensor <- sample(x = 0:1, size = N, replace = TRUE, prob = c('0' = 0.5, '1' = 0.5))
-pattern <- AutomataMatrix(iterations = 25, tensor = tensor, FUN = AutomataRandomRule)
-
-print(pattern)
-image(pattern, col = c('white', 'black'), axes = FALSE)
-
-
-
-# Testing AutomataSummationRule
+# AutomataSummationRule
 mapply(AutomataSummationRule,
        i = c(0, 0, 0, 0, 1, 1, 1, 1),
        j = c(0, 0, 1, 1, 0, 0, 1, 1),
        k = c(0, 1, 0, 1, 0, 1, 0, 1))
 
-N <- 49
-tensor <- sample(x = 0:1, size = N, replace = TRUE, prob = c('0' = 0.9, '1' = 0.1))
-pattern <- AutomataMatrix(iterations = 25, tensor = tensor, FUN = AutomataSummationRule)
-
-print(pattern)
-image(pattern, col = c('white', 'black'), axes = FALSE)
-
-
-
-
-
+# Try
+AutomataExperiment(N = 49, prob = c('0' = 0.95, '1' = 0.05), iterations = 25, FUN = AutomataSummationRule)
 
