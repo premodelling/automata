f6016081c1a44103ea5fd4a50b4b6d08d3bcca30
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 18:50:23 +0000

    Update R/functions/UpdateRowIndependent.R

--- R/functions/UpdateRowIndependent.R
+++ R/functions/UpdateRowIndependent.R
@@ -1,13 +1,19 @@
-# Title     : Update Row
+# Title     : Update Row Independent
 # Objective : Update automata rows independent of, without using, external function UpdateCell()
-# Created by: Think
+# Created by: 36112985
 # Created on: 15/11/2021
 
 
-#' @param tensor: the row vector that will update the next row
-#' @param FUN: the function of an automata rule type
+#' UpdateRowIndependent
+#'
+#' @description For creating the new/next row vector of a cellular automaton
+#'
+#' @param tensor: The row vector that will update the next row.
+#' @param FUN: The function of an automata rule.
+#'
+#' @return The new/next row vector, i.e., the cellular automaton's next generation row.
+#'
 UpdateRowIndependent <- function (tensor, FUN) {
-
 
   # The length of the tensor (N) and a
   # cyclically shifted vector of indices (R) ... it ensures that index - 1 picks the last ... if index == 1
