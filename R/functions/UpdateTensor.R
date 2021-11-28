df41aed3a74696efcfc8c4e4823989959753fb40
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 18:50:34 +0000

    update R/functions/UpdateTensor.R

--- R/functions/UpdateTensor.R
+++ R/functions/UpdateTensor.R
@@ -1,12 +1,21 @@
 # Title     : Update Tensor
 # Objective : For testing the row updating functions UpdateRowIndependent() & UpdateRowDependent()
-# Created by: Think
+# Created by: 36112985
 # Created on: 28/11/2021
 
 
+#' UpdateTensor
+#'
+#' @description For testing the row updating functions UpdateRowIndependent() & UpdateRowDependent()
+#'
 #' @param iterations: the number of rows, including 'tensor', to preview
 #' @param tensor: the row vector that will update the next row
 #' @param FUN: the function of an automata rule type
+#'
+#' @note Only prints a pattern, no return
+#'
+#' @return
+#'
 UpdateTensor <- function (iterations, tensor, FUN) {
 
   T <- tensor
