89cb39589a892dd752485be34ec3eaa7056bd0a9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 18:26:09 +0000

    Update R/functions/AutomataModuloRule.R

--- R/functions/AutomataModuloRule.R
+++ R/functions/AutomataModuloRule.R
@@ -3,6 +3,14 @@
 # Created by: Think
 # Created on: 16/11/2021
 
+
+#' @param i: The state of the first of three contiguous cells of a row vector.
+#' @param j: The state of the second of three contiguous cells of a row vector.
+#' @param k: The state of the third of three contiguous cells of a row vector.
+#'
+#' @return: The state of the new generation cell that is adjacent to the three cells.  A new generation cell
+#'          is a cell in the new/next row down.
+#'
 AutomataModuloRule <- function (i, j, k) {
 
   if ( any( !( c(i, j, k) %in% c(0, 1) ) ) ) {
