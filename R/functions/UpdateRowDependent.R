92c7dd753a790f648a80115d28575e7285ec5345
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 11:56:01 +0000

    Update R/functions/UpdateRowDependent.R

--- R/functions/UpdateRowDependent.R
+++ R/functions/UpdateRowDependent.R
@@ -1,9 +1,12 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Update Row Dependent
+# Objective : Update automata rows via the UpdateCell() function
 # Created by: Think
 # Created on: 16/11/2021
 
-UpdateTensor <- function (tensor, FUN) {
+
+#' @param tensor: the row vector that will update the next row
+#' @param FUN: the function of an automata rule type
+UpdateRowDependent <- function (tensor, FUN) {
 
   return (mapply(UpdateCell,
          index = seq(from = 1, to = length(tensor)),
