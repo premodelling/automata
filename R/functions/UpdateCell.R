019b46b6038be418fe737129e8bfc51011b4877d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 18:49:58 +0000

    Update R/functions/UpdateCell.R

--- R/functions/UpdateCell.R
+++ R/functions/UpdateCell.R
@@ -1,8 +1,19 @@
 # Title     : Update Cell
 # Objective : Update Cell
-# Created by: Think
+# Created by: 36112985
 # Created on: 15/11/2021
 
+
+#' UpdateCell
+#'
+#' @description For determining the state of a new/next generation cell
+#'
+#' @param index: The row index of the next generation cell whose state is being determined
+#' @param tensor: The row vector that is updating the cells of the next generation.
+#' @param FUN: The function of an automata rule.
+#'
+#' @return The state of a next generation cell
+#'
 UpdateCell <- function (index, tensor, FUN) {
 
   N <- length(tensor)
