606fbb1914a47b483ec0ae0fef5cc43ba3e26b9a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 19:10:10 +0000

    Update R/functions/AutomataExperiment.R

--- R/functions/AutomataExperiment.R
+++ R/functions/AutomataExperiment.R
@@ -1,13 +1,19 @@
 # Title     : Automata Experiment
 # Objective : Draw automata patterns w.r.t. a provided rule and other parameters
-# Created by: Think
+# Created by: 36112985
 # Created on: 28/11/2021
 
 
-#' @param N: row length
-#' @param prob: distribution of 0 & 1 in the starting row ... example -> c('0' = 0.95, '1' = 0.05)
-#' @param iterations: the number of automata matrix rows, including the starting row
-#' @param FUN: an automata rule
+#' AutomataExperiment
+#'
+#' @description Creates the image of a cellular automaton matrix w.r.t. an automaton rule function, a
+#'              starting row vector, and an iterations value that dictates the number of
+#'              automata arrays.
+#'
+#' @param N: the row vector length.
+#' @param prob: The distribution of 0 & 1 in the starting row ... e.g. c('0' = 0.95, '1' = 0.05)
+#' @param iterations: The number of automata matrix rows, including the starting row.
+#' @param FUN: An automaton rule.
 AutomataExperiment <- function (N, prob, iterations, FUN) {
 
   tensor <- sample(x = 0:1, size = N, replace = TRUE, prob = prob)
